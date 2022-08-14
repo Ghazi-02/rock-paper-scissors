23c411682a00f795ff8b44f16e7152b02146de23
Areeb <auhmirza02@gmail.com>
2022-08-14 16:19:34 +0100

    added reset button and win/loss/draw counter

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
+const result = document.querySelector('.result');
+result.classList.toggle('result');
+const rounds =document.querySelector('.rounds')
+rounds.classList.toggle('rounds')
 
 const rock = 0;
 const paper = 1;
 const scissors = 2
-let playerScore = 0;
+let playerScore =0;
 let computerScore = 0;
 let tie = 0;
-
-
-  
-function getPlayerChoice(playerSelection) {
-         
-     if (playerSelection === 0) {
-            return rock;
-        } else if (playerSelection === 1) {
-            return paper;
-        } else if (playerSelection === 2) {
-             return scissors;
-        } 
-    }
-
-
-
+let round = 0;
 
     function getComputerChoice() {
 
@@ -37,87 +26,85 @@ function getPlayerChoice(playerSelection) {
     }
  function playRound(playerChoice, computerChoice) {
 
-
+   
+   rounds.textContent=`Round: ${ round}  Wins: ${playerScore}  Losses: ${computerScore}  Draws: ${tie}`
+   
+ if(playerScore > computerScore && round > 4){
+    rounds.textContent="YOU HAVE BEATEN THE GAME!!!\nREFRESH TO TRY AGAIN";
+    
+  }else if(computerScore > playerScore && round > 4){
+    rounds.textContent="YOU HAVE LOST THE GAME!\nREFRESH TO TRY AGAIN";
      
-    if( playerChoice === 0 && computerChoice ===0 ){
+
+  }else if(computerScore === playerScore && round > 4){
+    rounds.textContent="THE GAME IS A DRAW!!\nREFRESH TO TRY AGAIN";
+}
+   
+   else if( playerChoice === 0 && computerChoice ===0 ){
+    round= round+1;   
+    tie = tie+1;
         return "Player chose: ROCK, CPU chose: ROCK \n!!DRAW!!"
     }else  if( playerChoice === 1 && computerChoice ===1 ){
-        tie = tie+1;
+        round= round+1;
+        tie = tie+1;        
         return "Player chose: PAPER, CPU chose: PAPER \n!!DRAW!!"
     }else  if( playerChoice === 2 && computerChoice === 2 ){
+        round=round+1;
         tie = tie+1;
         return "Player chose: SCISSORS, CPU chose: SCISSORS. \n!!DRAW!!"
     }
     else if( playerChoice === 0 && computerChoice === 2 ){
+        round=round+1;
         playerScore = playerScore+1;
         return "Player chose: ROCK, CPU chose : SCISSORS. \n!!!YOU WIN!!!"
     }else if( playerChoice === 1 && computerChoice === 0){
+        round=round+1;
         playerScore = playerScore+1;
         return "Player chose: PAPER, CPU chose: ROCK. \n!!!YOU WIN!!!"
     }else if( playerChoice === 2 && computerChoice === 1){
+        round=round+1;
         playerScore = playerScore+1;
         return "Player chose: SCISSORS, CPU chose: PAPER.\n!!!YOU WIN!!!"
     }
     else if( playerChoice === 0 && computerChoice === 1){
+        round=round+1;
         computerScore = computerScore+1;
         return "Player chose: ROCK, CPU chose: PAPER. \n!YOU LOST!"
     }else if( playerChoice === 1 && computerChoice === 2){
+        round=round+1;
         computerScore = computerScore+1;
         return "Player chose: PAPER, CPU chose: SCISSORS.\n!YOU LOST!"
     }else if( playerChoice === 2 && computerChoice === 0){
+        round=round+1;
         computerScore = computerScore+1;
         return "Player chose: SCISSORS, CPU chose: ROCK. \n!YOU LOST!"
-    }
-         
-  
+    }}
 
- }
+
+
  
 
 function game(){
  
-    
-  
-   for(let round = 1; round < 6; round++){
-   
-    
-    console.log("Round: " + round, " Wins: " + playerScore, " Losses: " + computerScore, " Draws: " + tie,);
-    if(playerScore > computerScore && round > 4){
-      return console.log("YOU HAVE BEATEN THE GAME!!!\nREFRESH TO TRY AGAIN");
-      
-    }else if(computerScore > playerScore && round > 4){
-       return console.log("YOU HAVE LOST THE GAME!\nREFRESH TO TRY AGAIN");
-       
- 
-    }else if(computerScore === playerScore && round > 4){
-        return console.log("THE GAME IS A DRAW!!\nREFRESH TO TRY AGAIN");
-  }
- }   
-}
-
-
-
 const btnRock= document.querySelector('#rock');
 const btnPaper= document.querySelector('#paper');
 const btnScissors= document.querySelector('#scissors');
     
       
-        btnRock.addEventListener('click', ()=>{
-   
-            result.textContent=playRound(rock,getComputerChoice());
-   
-   });btnPaper.addEventListener('click', ()=>{
-
-    result.textContent=playRound(paper,getComputerChoice());
+    btnRock.addEventListener('click', ()=>{
+        result.textContent=playRound(rock,getComputerChoice());
+ });btnPaper.addEventListener('click', ()=>{
+        result.textContent=playRound(paper,getComputerChoice());
  
-    });btnScissors.addEventListener('click', ()=>{
-   
+ });btnScissors.addEventListener('click', ()=>{
         result.textContent= playRound(scissors,getComputerChoice());
      
-    });
+ });
+  
+}
 
 
-    
-const result = document.querySelector('.result');
-result.classList.toggle('result');
+game()
 
+function reset(){ rounds.textContent=`Round: ${ round=0}  Wins: ${playerScore=0}  Losses: ${computerScore=0}  Draws: ${tie=0}`
+}
